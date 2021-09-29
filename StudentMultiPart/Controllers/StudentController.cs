de727bc0004a7761b7a00cd8426372f0e5d9cc9b
Muhammadharis322 <harisazam322@gmail.com>
2021-09-29 22:22:44 +0500

    Add project files.

---
+++ StudentMultiPart/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentMultiPart.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentMultiPart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpPost("api/student")]
+        public ActionResult Student([FromForm] StudentModel std)
+        {
+            // Getting Name
+            string name = std.Name;
+            // Getting Image
+            var image = std.Image;
+            // Saving Image on Server
+            var filePath = Path.Combine("wwwroot/images", image.FileName);
+            if (image.Length > 0)
+            {
+                
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    image.CopyTo(fileStream);
+                    
+                }
+            }
+
+            return Ok(new { status = true, message = "Student Posted Successfully" });
+        }
+    }
+}
---
+++ StudentMultiPart/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace StudentMultiPart.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
